4dafbd0970eb83f7eb252370e27856e64308c07e
programacion <damharnina25@iesarroyoharnina.es>
2024-11-29 10:02:38 +0100

    spring mvc

---
+++ primerProy/src/main/java/com/adorno/primerProy/PrimerProyApplication.java
@@ -0,0 +1,13 @@
+package com.adorno.primerProy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyApplication.class, args);
+	}
+
+}
---
+++ primerProy/src/test/java/com/adorno/primerProy/PrimerProyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adorno.primerProy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerProyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
