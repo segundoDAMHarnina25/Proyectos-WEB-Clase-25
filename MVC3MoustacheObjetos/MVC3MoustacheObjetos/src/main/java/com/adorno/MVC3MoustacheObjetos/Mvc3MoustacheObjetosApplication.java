ebb0cb6e81daae5edb0a051bb042ce7c48b1c2cb
programacion <damharnina25@iesarroyoharnina.es>
2024-12-17 14:02:31 +0100

    mustache 2 y algo

---
+++ MVC3MoustacheObjetos/MVC3MoustacheObjetos/src/main/java/com/adorno/MVC3MoustacheObjetos/Mvc3MoustacheObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.adorno.MVC3MoustacheObjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc3MoustacheObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc3MoustacheObjetosApplication.class, args);
+	}
+
+}
---
+++ MVC3MoustacheObjetos/MVC3MoustacheObjetos/src/test/java/com/adorno/MVC3MoustacheObjetos/Mvc3MoustacheObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adorno.MVC3MoustacheObjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc3MoustacheObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
