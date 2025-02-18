2c207e4fc939c565e60f2bff56f28fbfb3b078cf
programacion <damharnina25@iesarroyoharnina.es>
2025-02-18 14:06:40 +0100

    Update RolePopulates.java

--- SecurityStateLessPRSR1325/src/main/java/com/adorno/populaters/RolePopulates.java
+++ SecurityStateLessPRSR1325/src/main/java/com/adorno/populaters/RolePopulates.java
@@ -4,16 +4,21 @@ import org.springframework.stereotype.Component;
 
 import com.adorno.model.ERole;
 import com.adorno.model.RoleEntity;
+import com.adorno.model.dtos.UserCreateDTO;
 import com.adorno.repositories.RoleRepository;
+import com.adorno.services.UserService;
 
 @Component
 public class RolePopulates {
 	private final RoleRepository roleRepository;
+	private final UserService userService;
 
-	public RolePopulates(RoleRepository roleRepository) {
+	public RolePopulates(RoleRepository roleRepository, UserService userService) {
 		super();
 		this.roleRepository = roleRepository;
+		this.userService=userService;
 		populateRoles();
+		populateUsers();
 	}
 
 	public void populateRoles() {
@@ -21,5 +26,9 @@ public class RolePopulates {
 			if (roleRepository.findByName(erole).isEmpty())
 				roleRepository.save(new RoleEntity(erole));
 		}
+	}
+	public void populateUsers() {
+		UserCreateDTO userCreateDTO = new UserCreateDTO("duque@dolor.es", "cayetano", "1Zzzzzz;", "ADMIN","USER");
+		userService.createUser(userCreateDTO);
 	}
 }
